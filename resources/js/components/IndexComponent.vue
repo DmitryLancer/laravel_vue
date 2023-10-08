4aee4be4561087ea871ec4075d9c371c2ebdd1cc
Dmitry <dmitrylancer.it@yandex.ru>
2023-10-08 21:14:53 +0300

    обновление пользователей

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -8,15 +8,26 @@
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
+                <th scope="col">Edit</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="person in people">
+            <template v-for="person in people">
+            <tr>
                 <th scope="row">{{ person.id }}</th>
                 <td>{{ person.name }}</td>
                 <td>{{ person.age }}</td>
                 <td>{{ person.job }}</td>
+                <td><a href="#" @click.prevent="changeEditPersonId(person.id)" class="btn btn-success">Edit</a></td>
             </tr>
+                <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                    <th scope="row">{{ person.id }}</th>
+                    <td><input type="text" class="form-control"></td>
+                    <td><input type="number" class="form-control"></td>
+                    <td><input type="text" class="form-control"></td>
+                    <td><a href="#" @click.prevent="changeEditPersonId(null)" class="btn btn-success">Update</a></td>
+                </tr>
+            </template>
             </tbody>
         </table>
     </div>
@@ -31,6 +42,7 @@ export default {
     data() {
         return {
             people: null,
+            editPersonId: null,
         }
     },
 
@@ -44,7 +56,16 @@ export default {
             .then( res => {
                this.people = res.data;
             })
-        }
+        },
+
+        changeEditPersonId(id) {
+            this.editPersonId = id
+        },
+
+        isEdit(id) {
+            return this.editPersonId === id
+        },
+
     },
 
 }
