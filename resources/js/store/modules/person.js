544a4939287023d62e5bbec147211fc92ef69d7a
Dmitry <dmitrylancer.it@yandex.ru>
2023-10-22 21:51:47 +0300

    не работают кнопки и роутинг

--- resources/js/store/modules/person.js
+++ resources/js/store/modules/person.js
@@ -1,10 +1,22 @@
+
+
 const state = {
-    person: null
+    person: {
+        name: null,
+        age: null,
+        job: null,
+    },
+    people: null,
 }
 
 
 const getters = {
-    person: state => state.person
+    person: state => state.person,
+    people: state => state.people,
+
+    isDisabled: () => {
+        return state.person.name && state.person.age && state.person.job
+    },
 }
 
 const actions = {
@@ -14,12 +26,45 @@ const actions = {
                 commit('setPerson', res.data.data)
             })
     },
+
+    getPeople({commit}) {
+        axios.get('/api/people')
+            .then(res => {
+                commit('setPeople', res.data.data)
+                this.people = res.data.data
+            })
+    },
+
+    deletePerson({dispatch}, id) {
+        axios.delete(`/api/people/${id}`)
+            .then(res => {
+                dispatch('getPeople')
+            })
+    },
+
+    update({}, data) {
+        axios.patch(`/api/people/${data.id}`, { name: data.name, age: data.age, job: data.job } )
+            .then( res => {
+                this.$router.push({name: 'person.show', params: {id: data.id}})
+            })
+    },
+
+    store({}, data) {
+        axios.post('/api/people', { name: data.name, age: data.age, job: data.job })
+            .then( res => {
+                this.$router.push({name: 'person.index'})
+            })
+    },
 }
 
 const mutations = {
     setPerson(state, person) {
         state.person = person
-    }
+    },
+
+    setPeople(state, people) {
+        state.people = people
+    },
 }
 
 
